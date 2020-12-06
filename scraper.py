7a4bb3edf4a7132cf79b2888accf1a7d33008829
bessam-haj-salem <bessamhajsalem@gmail.com>
2020-12-06 10:29:07 +0100

    setting environment variable for the password

--- scraper.py
+++ scraper.py
@@ -2,11 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
+import os
+
 
 URL = 'https://www.amazon.de/Mark-III-mit-24-105-ILCE7M3GBDI/dp/B07RCHY5M4/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=sony+a7&qid=1607199872&quartzVehicle=5-672&replacementKeywords=sony&sr=8-1'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
+
+password = os.environ.get('LOGINSERVER')
 
 
 def check_price():
@@ -37,7 +41,7 @@ def send_mail():
     server.starttls()  # encrypt email
     server.ehlo()
 
-    server.login('bessamhajsalem@gmail.com', 'uaxarepnsgvtictm')
+    server.login('bessamhajsalem@gmail.com', password)
 
     subject = 'Price fell down!'
     body = 'check the amazon link https://www.amazon.de/Mark-III-mit-24-105-ILCE7M3GBDI/dp/B07RCHY5M4/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=sony+a7&qid=1607199872&quartzVehicle=5-672&replacementKeywords=sony&sr=8-1'
@@ -58,3 +62,4 @@ check_price()
 # while True:
 #     check_price()
 #     time.sleep(86400)
+
